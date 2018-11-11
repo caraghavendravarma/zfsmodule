3f55c49b6ffe282a3603c36b93c7b168d583e53a
caraghavendravarma <caraghavendravarma@gmail.com>
2018-11-11 08:57:51 +0530

    added method in zfsmodule.py script for sending zfs snapshot

--- zfsmodules.py
+++ zfsmodules.py
@@ -9,4 +9,22 @@ data, err = a.communicate()
 
 raiddisks = re.findall(r"\b(sd+[a-z])\b", data.decode())
 
+
+def zfs_send(source, remoteip, destination):
+    zfssendstatus = " "
+    try:
+	cmd = "zfs send {} | ssh {} zfs recv {}".format(source, remoteip, destination)
+	if source and remoteip and destination:
+		data = subprocess.Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
+		out, err = data.communicate()
+		if not out and not err:
+			zfssendstatus = "success"
+		elif err:
+			zfssendstatus = "Failed"
+	else:
+		zfssendstatus = "Failed"
+    except:
+	zfssendstatus = "unexpected err"
+    return zfssendstatus
+
 print(raiddisks)
